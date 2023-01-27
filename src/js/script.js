82be281dde182da7994c547d3f0e1724711cbd6c
Kateryna Kuzmina <designkuzmina@gmail.com>
2023-01-27 22:14:52 +0100

    add opinions.scss and footer.scss and optimization of blocks

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
         adaptiveHeight: true,
+        variableWidth: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
         responsive: [
@@ -9,7 +10,9 @@ $(document).ready(function(){
                 breakpoint: 768,
                 settings: {
                     dots: true,
-                    arrows: false
+                    dotsClass: 'my-dots',
+                    arrows: false,
+                    centerPadding: '40px'
                 }
               },
         ]
